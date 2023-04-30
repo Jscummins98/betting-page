58fcf6a810f718fa56f7efe27fa6b79884f5ab7a
Jackson Cummins <jscummins98@gmail.com>
2023-04-30 13:46:11 -0400

    add css to edit result column and update for April 29th

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let team
 for (let i = 0; i < teamArray.length; i++) {
   currentCell = teamArray[i]
   team = currentCell.textContent
-  if (team === 'Vegas' || team === 'Dallas' || team === 'Carolina') {
+  if (team === 'Vegas' || team === 'Dallas' || team === 'Carolina' || team === 'Toronto' || team === 'Edmonton') {
     currentCell.classList.add('winning-team')
   } else if (team === 'test') {
     currentCell.classList.add('losing-team')
